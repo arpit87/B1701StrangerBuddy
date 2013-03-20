a90cbc7d2b8a2cf02cdd2b703e6585316f7c0695
arpit87 <arpit87@gmail.com>
2013-03-20 11:43:12 -0700

    updated nearbyuser group

--- Hopin/src/my/b1701/SB/Users/NearbyUserGroup.java
+++ Hopin/src/my/b1701/SB/Users/NearbyUserGroup.java
@@ -1,6 +1,8 @@
 package my.b1701.SB.Users;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import my.b1701.SB.LocationHelpers.SBGeoPoint;
@@ -9,18 +11,27 @@ import com.google.android.maps.GeoPoint;
 
 public class NearbyUserGroup {
 	
-	public List <NearbyUser> mUsersListInGroup;
-	public SBGeoPoint mGeoPoint;
+	public List <NearbyUser> mUsersListInGroup = Collections.emptyList();
+	public SBGeoPoint mGeoPoint = null;
 	public List<String> fbids;
+	int lat = 0;
+	int longi = 0;
 	
 	NearbyUserGroup(List<NearbyUser> nearbyUsers)
 	{
 		mUsersListInGroup = nearbyUsers;
 		fbids = new ArrayList<String>();
+		lat = nearbyUsers.get(0).getUserLocInfo().getGeoPoint().getLatitudeE6();
+		longi = nearbyUsers.get(0).getUserLocInfo().getGeoPoint().getLongitudeE6();
 		
 		for(NearbyUser n:nearbyUsers)
-			fbids.add(n.getUserFBInfo().getFbid());
-			
+		{
+			fbids.add(n.getUserFBInfo().getFbid());			
+			lat = (lat + n.getUserLocInfo().getGeoPoint().getLatitudeE6())/2;
+			longi = (longi + n.getUserLocInfo().getGeoPoint().getLongitudeE6())/2;			
+		}
+		
+		mGeoPoint = new SBGeoPoint(lat, longi);	
 	}
 	
 	public int getNumberOfUsersInGroup()
@@ -53,4 +64,17 @@ public class NearbyUserGroup {
 		return mUsersListInGroup.get(i);
 	}
 
+	public void addUserToGroup(NearbyUser n)
+	{
+		if(mUsersListInGroup.isEmpty())
+		{
+			mUsersListInGroup = new ArrayList<NearbyUser>();
+			lat = n.getUserLocInfo().getGeoPoint().getLatitudeE6();
+			longi = n.getUserLocInfo().getGeoPoint().getLongitudeE6();
+		}
+		lat = (lat + n.getUserLocInfo().getGeoPoint().getLatitudeE6())/2;
+		longi = (longi + n.getUserLocInfo().getGeoPoint().getLongitudeE6())/2;			
+		mGeoPoint = new SBGeoPoint(lat, longi);		
+		mUsersListInGroup.add(n);		
+	}
 }
